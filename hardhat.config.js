3db2a874bc952c2a1117cb2c2328cfc704706906
Jimmy Chung <jimmy46@hotmail.ca>
2021-11-22 02:20:00 -0500

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.0",
+ module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
